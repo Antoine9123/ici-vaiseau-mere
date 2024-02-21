986784c8dace8201a77cd3e5c3b77c9c420b2ac0
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-02-21 20:24:45 +0100

    Artiste list showing + issues left

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
 
-function showContent(contentId) {
+
+function showSubmenu(contentId) {
   var contents = document.querySelectorAll(".primary-title");
   contents.forEach(function (content) {
     content.style.display = "none";
@@ -12,53 +12,92 @@ function showContent(contentId) {
   }
 }
 
+// function showContent(contentId) {
+//   var contents = document.querySelectorAll(".content");
+//   contents.forEach(function (content) {
+//     content.style.display = "none";
+//   });
+
+//   var selectedContent = document.getElementById(contentId);
+//   if (selectedContent) {
+//     selectedContent.style.display = "block";
+//   }
+// }
+
+let pathfile = "database/artists.json";
+
+
 class ArtistManager {
-  static pathfile = "database/artists.json";
+    static async generateTable() {
+      const section = document.querySelector(".content");
+      const response = await fetch(pathfile);
+      const artists = await response.json();
+  
+      const headers = ["Name", "Project", "Art Fields"];
+      const table = document.createElement("table");
+  
+      const headerRow = document.createElement("tr");
+      headers.forEach((header) => {
+        const th = document.createElement("th");
+        th.innerText = header;
+        headerRow.appendChild(th);
+      });
+      table.appendChild(headerRow);
+  
+      artists.forEach((artist) => {
+        const row = document.createElement("tr");
+  
+        const name = document.createElement("td");
+        const project = document.createElement("td");
+        const artFields = document.createElement("td");
 
-  static createArtist(
-    name,
-    projectName,
-    artFields,
-    shortDesc,
-    longDesc,
-    thumbnail,
-    picture,
-    mail,
-    facebook,
-    instagram,
-    website
-  ) {
-    let newArtist = {};
-    newArtist["name"] = name;
-    newArtist["projectName"] = projectName;
-    newArtist["artFields"] = artFields;
-    newArtist["shortDescription"] = shortDesc;
-    newArtist["longDescription"] = longDesc;
-    newArtist["thumbnail"] = thumbnail;
-    newArtist["picture"] = picture;
-    newArtist["mail"] = mail;
-    newArtist["facebook"] = facebook;
-    newArtist["instagram"] = instagram;
-    newArtist["website"] = website;
-
-    const existingData = fs.readFileSync(ArtistManager.pathfile, "utf-8");
-    const existingArtists = JSON.parse(existingData);
-    console.log(existingArtists)
-    existingArtists.push(newArtist);
-    const updatedData = JSON.stringify(existingArtists, null, 2);
-    fs.writeFileSync(ArtistManager.pathfile, updatedData, "utf-8");
+        name.innerText = artist["name"]; 
+        project.innerText = artist["projectName"]; 
+        artFields.innerText = artist["artFields"]; 
+       
+        row.appendChild(name);
+        row.appendChild(project);       
+        row.appendChild(artFields);
+  
+        table.appendChild(row);
+      });
+      section.appendChild(table);
+    }
   }
-}
+  
 
-// ArtistManager.createArtist(
-//     "Nicolas", 
-//     "Bourlingueur du dimanche",
-//     "chanteur",
-//     "Courte chansonnette",
-//     "Ca fait lonftemps que je l'attends, cette princesse aux yeux d'or",
-//     "tinytiny",
-//     "pictureguitar.jpg",
-//     "nico@mail.com",
-//     "nico.fb",
-//     "nico.insta",
-//     "nico.be")
+
+
+// function createArtist(
+//   name,
+//   projectName,
+//   artFields,
+//   shortDesc,
+//   longDesc,
+//   thumbnail,
+//   picture,
+//   mail,
+//   facebook,
+//   instagram,
+//   website
+// ) {
+//   let newArtist = {};
+//   newArtist["name"] = name;
+//   newArtist["projectName"] = projectName;
+//   newArtist["artFields"] = artFields;
+//   newArtist["shortDescription"] = shortDesc;
+//   newArtist["longDescription"] = longDesc;
+//   newArtist["thumbnail"] = thumbnail;
+//   newArtist["picture"] = picture;
+//   newArtist["mail"] = mail;
+//   newArtist["facebook"] = facebook;
+//   newArtist["instagram"] = instagram;
+//   newArtist["website"] = website;
+
+//   const existingData = fs.readFileSync(pathfile, "utf-8");
+//   const existingArtists = JSON.parse(existingData);
+//   console.log(existingArtists);
+//   existingArtists.push(newArtist);
+//   const updatedData = JSON.stringify(existingArtists, null, 2);
+//   fs.writeFileSync(pathfile, updatedData, "utf-8");
+// }
