799b95fa8dceab08e9ac97f7808d1e4b08c67e2a
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-02-27 19:32:47 +0100

    Add artist works

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const Artist = require("../models/artist");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("./admin/admin", { file: null });
+router.use(express.static("public"));
+router.use(express.urlencoded({ extended: true }));
+
+router.get("", (req, res) => {
+  res.render("./admin/admin", { content: null });
 });
 
 // router.get("/artists-list", (req, res) => {
@@ -10,7 +14,19 @@ router.get("/", (req, res) => {
 // });
 
 router.get("/artists-add", (req, res) => {
-  res.render("./admin/admin", { file: './partials/artists-add' });
+  res.render("./admin/admin", { content: './partials/artists-add' });
 });
+
+router.post("/artists-add", (req, res) => {
+  const artist = new Artist(req.body)
+
+  artist.save()
+    .then((result) => {
+      res.redirect('/')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 module.exports = router;
