c00bdefa0db64d77a26b2ac0477a6646d992153c
ndz <nicolas.dzierzgowski@gmail.com>
2024-04-03 14:42:53 +0200

    filter residencies added

--- public/scripts/filters.js
+++ public/scripts/filters.js
@@ -1,10 +1,37 @@
 const filterBtn = document.querySelectorAll(".filter-btn");
 const miniatures = document.querySelectorAll(".residency");
+const cross = document.querySelector(".cross")
 
-filterBtn.addEventListener('click', () => {
-    filterBtn.forEach((filter) =>{
+const resetColor = () => filterBtn.forEach((filter) =>{
+    filter.classList.remove("selected")
+    filter.classList.add("unselected")
+    })
+
+filterBtn.forEach((filter) =>{
+    let type = filter.textContent
+    filter.addEventListener('click', () => {
+        resetColor()
+        filter.classList.add("selected")
+        filter.classList.remove("unselected")
+        cross.style.display = "block";
         miniatures.forEach((miniature) =>{
-            if (filter.classList.contains())
+            if (miniature.classList.contains(type)){
+                miniature.style.display = "block"
+            }else{
+                miniature.style.display = "none"
+            }
         })
     })
 })
+
+
+cross.addEventListener("click", () => {
+    filterBtn.forEach((filter) => {
+        filter.classList.remove("selected");
+        filter.classList.add("unselected");
+    });
+    miniatures.forEach((miniature) =>{
+        miniature.style.display = "block";
+    });
+    cross.style.display = "none";
+});
