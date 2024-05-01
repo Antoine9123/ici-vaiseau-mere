124ca2d6bafb598c97fdcf2301bef8d2f8886f57
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-01 14:53:23 +0200

    Done done done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,12 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-
-
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  compression: "auto", 
+  default_image_quality: 'auto:good', 
 });
 
 module.exports = cloudinary;
